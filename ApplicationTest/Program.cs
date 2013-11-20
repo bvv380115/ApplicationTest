35bee192d72f4382e815dd2959e4366e193b9b52
bvv380115 <bvv380115@gmail.com>
2013-11-20 22:25:16 +0600

    x1

---
+++ ApplicationTest/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testprog
+{
+    class Program
+    {
+
+        public int method1()
+        {
+
+            int x1 = 0;
+
+            int a = 10, b = 10;
+
+            x1 = a + b;
+
+            return x1;
+        }
+
+
+
+
+        static void Main(string[] args)
+        {
+
+            Program x1 = new Program();
+
+            Console.WriteLine("x1 = " + x1.method1());
+
+         
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
